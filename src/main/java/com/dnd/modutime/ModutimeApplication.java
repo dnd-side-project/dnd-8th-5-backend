6e63d3611b00749f00cbd6ed35f54ad3ca39ad97
ssssujini99 <sssssjin99@naver.com>
2023-02-01 14:34:59 +0900

    chore: project settings

---
+++ src/main/java/com/dnd/modutime/ModutimeApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.modutime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModutimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModutimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnd/modutime/ModutimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.modutime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModutimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
