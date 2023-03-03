335609ceef5df591fef4ca7f1cb6d2ae0a630c02
ssssujini99 <sssssjin99@naver.com>
2023-03-03 15:10:23 +0900

    fix: 참여자 일정 조회 API 변경

--- src/main/java/com/dnd/modutime/dto/response/AvailableDateTimeResponse.java
+++ src/main/java/com/dnd/modutime/dto/response/AvailableDateTimeResponse.java
@@ -1,22 +1,24 @@
 package com.dnd.modutime.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class AvailableDateTimeResponse {
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
-    private LocalDate availableDate;
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+//    private LocalDate availableDate;
+//
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm", timezone = "Asia/Seoul")
+//    private List<LocalTime> availableTimes;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm", timezone = "Asia/Seoul")
-    private List<LocalTime> availableTimes;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    private LocalDateTime localDateTime;
 }
--- src/test/java/com/dnd/modutime/acceptance/TimeBlockAcceptanceTest.java
+++ src/test/java/com/dnd/modutime/acceptance/TimeBlockAcceptanceTest.java
@@ -1,22 +1,21 @@
 package com.dnd.modutime.acceptance;
 
-import static com.dnd.modutime.fixture.RoomRequestFixture.getRoomRequestNoTime;
-import static com.dnd.modutime.fixture.TimeFixture._12_00;
-import static com.dnd.modutime.fixture.TimeFixture._13_00;
-import static com.dnd.modutime.fixture.TimeFixture._2023_02_10;
-import static com.dnd.modutime.fixture.TimeFixture.getAvailableDateTimeRequest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-
 import com.dnd.modutime.dto.response.AvailableDateTimeResponse;
 import com.dnd.modutime.dto.response.RoomCreationResponse;
 import com.dnd.modutime.dto.response.TimeBlockResponse;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.dnd.modutime.fixture.RoomRequestFixture.getRoomRequestNoTime;
+import static com.dnd.modutime.fixture.TimeFixture.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class TimeBlockAcceptanceTest extends AcceptanceSupporter {
 
@@ -43,7 +42,7 @@ public class TimeBlockAcceptanceTest extends AcceptanceSupporter {
     }
 
     @Test
-    void 참여자가_등록한_시간을_조회한다() {
+    void 참여자가_등록한_날짜와_시간을_조회한다() {
         RoomCreationResponse roomCreationResponse = 방_생성();
         String participantName = "참여자1";
         로그인_참여자_1234(roomCreationResponse.getUuid(), participantName);
@@ -52,16 +51,36 @@ public class TimeBlockAcceptanceTest extends AcceptanceSupporter {
 
         ExtractableResponse<Response> response = get("/api/room/" + roomCreationResponse.getUuid() + "/available-time?name=" + participantName);
         TimeBlockResponse timeBlockResponse = response.body().as(TimeBlockResponse.class);
+
         assertAll(
                 () -> assertThat(timeBlockResponse.getName()).isEqualTo(participantName),
                 () -> assertThat(timeBlockResponse.getAvailableDateTimes().stream()
-                        .map(AvailableDateTimeResponse::getAvailableDate)
-                        .collect(Collectors.toList())).hasSize(1)
-                        .contains(_2023_02_10),
-                () -> assertThat(timeBlockResponse.getAvailableDateTimes().stream()
-                        .flatMap(it -> it.getAvailableTimes().stream())
+                        .map(AvailableDateTimeResponse::getLocalDateTime)
                         .collect(Collectors.toList())).hasSize(2)
-                        .contains(_12_00, _13_00)
+                        .contains(LocalDateTime.of(2023, 2, 10, 12, 0), LocalDateTime.of(2023, 2, 10, 13, 0))
+        );
+    }
+
+    @Test
+    void 참여자가_등록한_날짜을_조회한다() {
+        RoomCreationResponse roomCreationResponse = 방_생성(getRoomRequestNoTime());
+        String participantName = "참여자1";
+        로그인_참여자_1234(roomCreationResponse.getUuid(), participantName);
+        시간을_등록한다(roomCreationResponse.getUuid(), participantName, getAvailableDateTimeRequest(
+                _2023_02_10, null));
+
+        ExtractableResponse<Response> response = get("/api/room/" + roomCreationResponse.getUuid() + "/available-time?name=" + participantName);
+        TimeBlockResponse timeBlockResponse = response.body().as(TimeBlockResponse.class);
+
+        AvailableDateTimeResponse availableDateTimeResponse = timeBlockResponse.getAvailableDateTimes().get(0);
+        System.out.println(availableDateTimeResponse);
+
+        assertAll(
+                () -> assertThat(timeBlockResponse.getName()).isEqualTo(participantName),
+                () -> assertThat(timeBlockResponse.getAvailableDateTimes().stream()
+                        .map(AvailableDateTimeResponse::getLocalDateTime)
+                        .collect(Collectors.toList())).hasSize(1)
+                        .contains(LocalDateTime.of(2023, 2, 10, 0, 0))
         );
     }
 }
