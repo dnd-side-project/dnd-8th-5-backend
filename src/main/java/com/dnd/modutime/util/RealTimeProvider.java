8ae1b032c94acca8f63caa9599bb8ee5016649a7
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-06-26 04:20:54 +0900

    [fix] : LocalDateTime에서 utc 시간 설정

--- src/main/java/com/dnd/modutime/util/RealTimeProvider.java
+++ src/main/java/com/dnd/modutime/util/RealTimeProvider.java
@@ -1,12 +1,16 @@
 package com.dnd.modutime.util;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RealTimeProvider implements TimeProvider {
     @Override
     public LocalDateTime getCurrentLocalDateTime() {
-        return LocalDateTime.now();
+        ZoneOffset koreaZoneOffset = ZoneOffset.of("+09:00");
+        return ZonedDateTime.now(koreaZoneOffset).toLocalDateTime();
     }
 }
