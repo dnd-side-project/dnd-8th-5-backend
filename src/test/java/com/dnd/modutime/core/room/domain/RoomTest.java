b97fa55ea152094bf9f05e9d1e9f7ed1a34ff406
수지니 <71487608+ssssujini99@users.noreply.github.com>
2023-07-27 15:45:04 +0900

    fix: endTime으로 24시 선택 시 오류 해결 및 교체 로직 수정 (#78)
    * fix: 방 생성시 startTime < endTime 검증 삭제 및 교체 로직 수정
    
    * feat: 시작시간>끝시간 인 경우 timetable 세팅에 대한 로직 추가
    
    * feat: roomFixture 추가
    
    * feat: acceptanceSupporter 추가
    
    * test: 시작시간>끝시간인 방 생성 인수테스트 추가
    
    * refactor: 코드리뷰 반영 및 리팩토링
    
    * refactor: 코드리뷰 반영 및 리팩토링
    
    * refactor: 코드리뷰 반영 및 리팩토링
    
    * refactor: includeTime 체크 시 time 기준으로 로직 변경

--- src/test/java/com/dnd/modutime/core/room/domain/RoomTest.java
+++ src/test/java/com/dnd/modutime/core/room/domain/RoomTest.java
@@ -5,25 +5,74 @@ import static com.dnd.modutime.fixture.RoomFixture.getRoomByHeadCount;
 import static com.dnd.modutime.fixture.RoomFixture.getRoomByRoomDates;
 import static com.dnd.modutime.fixture.RoomFixture.getRoomByStartEndTime;
 import static com.dnd.modutime.fixture.RoomFixture.getRoomByTitle;
+import static com.dnd.modutime.fixture.TimeFixture._00_00;
+import static com.dnd.modutime.fixture.TimeFixture._00_30;
+import static com.dnd.modutime.fixture.TimeFixture._01_00;
+import static com.dnd.modutime.fixture.TimeFixture._01_30;
+import static com.dnd.modutime.fixture.TimeFixture._02_00;
+import static com.dnd.modutime.fixture.TimeFixture._02_30;
+import static com.dnd.modutime.fixture.TimeFixture._03_00;
+import static com.dnd.modutime.fixture.TimeFixture._03_30;
+import static com.dnd.modutime.fixture.TimeFixture._04_00;
+import static com.dnd.modutime.fixture.TimeFixture._04_30;
+import static com.dnd.modutime.fixture.TimeFixture._05_00;
+import static com.dnd.modutime.fixture.TimeFixture._05_30;
+import static com.dnd.modutime.fixture.TimeFixture._06_00;
+import static com.dnd.modutime.fixture.TimeFixture._06_30;
+import static com.dnd.modutime.fixture.TimeFixture._07_00;
+import static com.dnd.modutime.fixture.TimeFixture._07_30;
+import static com.dnd.modutime.fixture.TimeFixture._08_00;
+import static com.dnd.modutime.fixture.TimeFixture._08_30;
+import static com.dnd.modutime.fixture.TimeFixture._09_00;
+import static com.dnd.modutime.fixture.TimeFixture._09_30;
+import static com.dnd.modutime.fixture.TimeFixture._10_00;
+import static com.dnd.modutime.fixture.TimeFixture._10_30;
 import static com.dnd.modutime.fixture.TimeFixture._11_00;
+import static com.dnd.modutime.fixture.TimeFixture._11_30;
 import static com.dnd.modutime.fixture.TimeFixture._12_00;
+import static com.dnd.modutime.fixture.TimeFixture._12_30;
 import static com.dnd.modutime.fixture.TimeFixture._13_00;
+import static com.dnd.modutime.fixture.TimeFixture._13_30;
 import static com.dnd.modutime.fixture.TimeFixture._14_00;
+import static com.dnd.modutime.fixture.TimeFixture._14_30;
+import static com.dnd.modutime.fixture.TimeFixture._15_00;
+import static com.dnd.modutime.fixture.TimeFixture._15_30;
+import static com.dnd.modutime.fixture.TimeFixture._16_00;
+import static com.dnd.modutime.fixture.TimeFixture._16_30;
+import static com.dnd.modutime.fixture.TimeFixture._17_00;
+import static com.dnd.modutime.fixture.TimeFixture._17_30;
+import static com.dnd.modutime.fixture.TimeFixture._18_00;
+import static com.dnd.modutime.fixture.TimeFixture._18_30;
+import static com.dnd.modutime.fixture.TimeFixture._19_00;
+import static com.dnd.modutime.fixture.TimeFixture._19_30;
 import static com.dnd.modutime.fixture.TimeFixture._2023_02_08;
 import static com.dnd.modutime.fixture.TimeFixture._2023_02_09;
 import static com.dnd.modutime.fixture.TimeFixture._2023_02_10;
 import static com.dnd.modutime.fixture.TimeFixture._2023_02_10_00_00;
 import static com.dnd.modutime.fixture.TimeFixture._2023_02_20_00_00;
+import static com.dnd.modutime.fixture.TimeFixture._20_00;
+import static com.dnd.modutime.fixture.TimeFixture._20_30;
+import static com.dnd.modutime.fixture.TimeFixture._21_00;
+import static com.dnd.modutime.fixture.TimeFixture._21_30;
+import static com.dnd.modutime.fixture.TimeFixture._22_00;
+import static com.dnd.modutime.fixture.TimeFixture._22_30;
+import static com.dnd.modutime.fixture.TimeFixture._23_00;
+import static com.dnd.modutime.fixture.TimeFixture._23_30;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import com.dnd.modutime.util.FakeTimeProvider;
-import com.dnd.modutime.core.room.domain.Room;
-import com.dnd.modutime.core.room.domain.RoomDate;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.dnd.modutime.util.FakeTimeProvider;
 
 public class RoomTest {
 
@@ -31,12 +80,6 @@ public class RoomTest {
     void 방이_생성되면_Uuid를_생성한다() {
         Room room = getRoom();
         assertThat(room.getUuid()).isNotNull();
-    }
-
-    @Test
-    void 방의_시작시간이_끝나는시간보다_나중이면_예외가_발생한다() {
-        assertThatThrownBy(() -> getRoomByStartEndTime(_13_00, _12_00))
-                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
@@ -159,5 +202,149 @@ public class RoomTest {
     void 끝나는시간의_이후_시간이_들어오면_예외가_발생한다() {
         Room room = getRoomByStartEndTime(_12_00, _13_00);
         assertThat(room.includeTime(_14_00)).isFalse();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideLocalTimeBetweenStartAndEndTime")
+    void 시작시간이_끝시간보다_큰_경우_사이값의_시간이_포함되면_예외가_발생한다(LocalTime invalidTime) {
+        Room room = getRoomByStartEndTime(_13_00, _11_00);
+        assertThat(room.includeTime(invalidTime)).isFalse();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideLocalTimeNotBetweenStartAndEndTime")
+    void 시작시간이_끝시간보다_큰_경우_사이값의_시간이_포함되지_않으면_예외가_발생하지_않는다(LocalTime validTime) {
+        Room room = getRoomByStartEndTime(_13_00, _11_00);
+        assertThat(room.includeTime(validTime)).isTrue();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideLocalTimeBetweenStartAndEndTimeWhenEndTimeIsZero")
+    void 끝시간이_00시이고_시작시간이_끝시간보다_큰_경우_사이값의_시간이_포함되면_예외가_발생한다(LocalTime invalidTime) {
+        Room room = getRoomByStartEndTime(_23_00, _00_00);
+        assertThat(room.includeTime(invalidTime)).isFalse();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideLocalTimeNotBetweenStartAndEndTimeWhenEndTimeIsZero")
+    void 끝시간이_00시이고_시작시간이_끝시간보다_큰_경우_사이값의_시간이_포함되지_않으면_예외가_발생하지_않는다(LocalTime validTime) {
+        Room room = getRoomByStartEndTime(_23_00, _00_00);
+        assertThat(room.includeTime(validTime)).isTrue();
+    }
+
+    private static Stream<Arguments> provideLocalTimeBetweenStartAndEndTime() {
+        return Stream.of(
+            Arguments.of(_11_00),
+            Arguments.of(_11_30),
+            Arguments.of(_12_00),
+            Arguments.of(_12_30)
+        );
+    }
+
+    private static Stream<Arguments> provideLocalTimeNotBetweenStartAndEndTime() {
+        return Stream.of(
+            Arguments.of(_13_00),
+            Arguments.of(_13_30),
+            Arguments.of(_14_00),
+            Arguments.of(_14_30),
+            Arguments.of(_15_00),
+            Arguments.of(_15_30),
+            Arguments.of(_16_00),
+            Arguments.of(_16_30),
+            Arguments.of(_17_00),
+            Arguments.of(_17_30),
+            Arguments.of(_18_00),
+            Arguments.of(_18_30),
+            Arguments.of(_19_00),
+            Arguments.of(_19_30),
+            Arguments.of(_20_00),
+            Arguments.of(_20_30),
+            Arguments.of(_21_00),
+            Arguments.of(_21_30),
+            Arguments.of(_22_00),
+            Arguments.of(_22_30),
+            Arguments.of(_23_00),
+            Arguments.of(_23_30),
+            Arguments.of(_00_00),
+            Arguments.of(_00_30),
+            Arguments.of(_01_00),
+            Arguments.of(_01_30),
+            Arguments.of(_02_00),
+            Arguments.of(_02_30),
+            Arguments.of(_03_00),
+            Arguments.of(_03_30),
+            Arguments.of(_04_00),
+            Arguments.of(_04_30),
+            Arguments.of(_05_00),
+            Arguments.of(_05_30),
+            Arguments.of(_06_00),
+            Arguments.of(_06_30),
+            Arguments.of(_07_00),
+            Arguments.of(_07_30),
+            Arguments.of(_08_00),
+            Arguments.of(_08_30),
+            Arguments.of(_09_00),
+            Arguments.of(_09_30),
+            Arguments.of(_10_00),
+            Arguments.of(_10_30)
+        );
+    }
+
+    private static Stream<Arguments> provideLocalTimeBetweenStartAndEndTimeWhenEndTimeIsZero() {
+        return Stream.of(
+            Arguments.of(_00_00),
+            Arguments.of(_00_30),
+            Arguments.of(_01_00),
+            Arguments.of(_01_30),
+            Arguments.of(_02_00),
+            Arguments.of(_02_30),
+            Arguments.of(_03_00),
+            Arguments.of(_03_30),
+            Arguments.of(_04_00),
+            Arguments.of(_04_30),
+            Arguments.of(_05_00),
+            Arguments.of(_05_30),
+            Arguments.of(_06_00),
+            Arguments.of(_06_30),
+            Arguments.of(_07_00),
+            Arguments.of(_07_30),
+            Arguments.of(_08_00),
+            Arguments.of(_08_30),
+            Arguments.of(_09_00),
+            Arguments.of(_09_30),
+            Arguments.of(_10_00),
+            Arguments.of(_10_30),
+            Arguments.of(_11_00),
+            Arguments.of(_11_30),
+            Arguments.of(_12_00),
+            Arguments.of(_12_30),
+            Arguments.of(_13_00),
+            Arguments.of(_13_30),
+            Arguments.of(_14_00),
+            Arguments.of(_14_30),
+            Arguments.of(_15_00),
+            Arguments.of(_15_30),
+            Arguments.of(_16_00),
+            Arguments.of(_16_30),
+            Arguments.of(_17_00),
+            Arguments.of(_17_30),
+            Arguments.of(_18_00),
+            Arguments.of(_18_30),
+            Arguments.of(_19_00),
+            Arguments.of(_19_30),
+            Arguments.of(_20_00),
+            Arguments.of(_20_30),
+            Arguments.of(_21_00),
+            Arguments.of(_21_30),
+            Arguments.of(_22_00),
+            Arguments.of(_22_30)
+        );
+    }
+
+    private static Stream<Arguments> provideLocalTimeNotBetweenStartAndEndTimeWhenEndTimeIsZero() {
+        return Stream.of(
+            Arguments.of(_23_00),
+            Arguments.of(_23_30)
+        );
     }
 }
