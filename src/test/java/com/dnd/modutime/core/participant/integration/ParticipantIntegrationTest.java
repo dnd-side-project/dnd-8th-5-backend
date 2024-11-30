97cafd6dc959729ff6372b4eb13f0acc636bca3d
김동호 <dongho.dh.kim@kurlycorp.com>
2024-11-30 23:55:09 +0900

    test: 실패하는 테스트 해결

--- src/test/java/com/dnd/modutime/core/participant/integration/ParticipantIntegrationTest.java
+++ src/test/java/com/dnd/modutime/core/participant/integration/ParticipantIntegrationTest.java
@@ -1,19 +1,21 @@
 package com.dnd.modutime.core.participant.integration;
 
-import static com.dnd.modutime.fixture.RoomRequestFixture.ROOM_UUID;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-
 import com.dnd.modutime.core.participant.application.ParticipantService;
+import com.dnd.modutime.core.participant.repository.ParticipantRepository;
 import com.dnd.modutime.core.timeblock.application.ParticipantCreationEvent;
 import com.dnd.modutime.core.timeblock.domain.TimeBlock;
 import com.dnd.modutime.core.timeblock.repository.TimeBlockRepository;
-import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.event.ApplicationEvents;
 import org.springframework.test.context.event.RecordApplicationEvents;
+
+import java.util.Optional;
+
+import static com.dnd.modutime.fixture.RoomRequestFixture.ROOM_UUID;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest
 @RecordApplicationEvents
@@ -21,6 +23,9 @@ public class ParticipantIntegrationTest {
 
     @Autowired
     private ParticipantService participantService;
+
+    @Autowired
+    private ParticipantRepository participantRepository;
 
     @Autowired
     private TimeBlockRepository timeBlockRepository;
