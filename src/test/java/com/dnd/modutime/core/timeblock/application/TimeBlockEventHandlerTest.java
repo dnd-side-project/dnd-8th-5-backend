dda026afdf39af27f6a783558b3551546684982c
김동호 <dongho.dh.kim@kurlycorp.com>
2025-05-18 17:06:58 +0900

    test: TimeBlockService 메서드 호출 여부에 대한 검증 로직 추가

--- src/test/java/com/dnd/modutime/core/timeblock/application/TimeBlockEventHandlerTest.java
+++ src/test/java/com/dnd/modutime/core/timeblock/application/TimeBlockEventHandlerTest.java
@@ -1,6 +1,7 @@
 package com.dnd.modutime.core.timeblock.application;
 
 import com.dnd.modutime.core.participant.application.ParticipantService;
+import com.dnd.modutime.core.participant.domain.ParticipantRemovedEvent;
 import com.dnd.modutime.core.timeblock.domain.TimeBlock;
 import com.dnd.modutime.core.timeblock.domain.TimeBlockRemovedEvent;
 import com.dnd.modutime.core.timeblock.repository.TimeBlockRepository;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.event.ApplicationEvents;
 import org.springframework.test.context.event.RecordApplicationEvents;
 
@@ -16,6 +18,7 @@ import java.util.Optional;
 import static com.dnd.modutime.fixture.RoomRequestFixture.ROOM_UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 @RecordApplicationEvents
@@ -27,21 +30,26 @@ class TimeBlockEventHandlerTest {
     @Autowired
     private TimeBlockRepository timeBlockRepository;
 
+    @SpyBean
+    private TimeBlockService timeBlockService;
+
     @Autowired
     private ApplicationEvents events;
 
-    @DisplayName("참여자가 생성되면 참여자의 TimeBlock이 생성된다")
+    @DisplayName("참여자가 생성되면 참여자의 타임블록 생성이 호출된다.")
     @Test
     void 참여자가_생성되면_참여자의_TimeBlock이_생성된다() {
-        participantService.create(ROOM_UUID, "참여자1", "1234");
-        Optional<TimeBlock> actual = timeBlockRepository.findByRoomUuidAndParticipantName(ROOM_UUID, "참여자1");
+        var participantName = "참여자1";
+        participantService.create(ROOM_UUID, participantName, "1234");
+        Optional<TimeBlock> actual = timeBlockRepository.findByRoomUuidAndParticipantName(ROOM_UUID, participantName);
         assertAll(
                 () -> assertThat(actual.isPresent()).isTrue(),
-                () -> assertThat(events.stream(ParticipantCreationEvent.class).count()).isEqualTo(1)
+                () -> assertThat(events.stream(ParticipantCreationEvent.class).count()).isEqualTo(1),
+                () -> verify(timeBlockService).create(ROOM_UUID, participantName)
         );
     }
 
-    @DisplayName("참여자가 삭제되면 타임블록도 삭제된다.")
+    @DisplayName("참여자가 삭제되면 타임블록도 삭제가 호출된다.")
     @Test
     void test01() {
         var participantName = "참여자1";
@@ -51,6 +59,10 @@ class TimeBlockEventHandlerTest {
         participantService.delete(ROOM_UUID, participantName);
 
         // then
-        assertThat(events.stream(TimeBlockRemovedEvent.class).count()).isEqualTo(1);
+        assertAll(
+                () -> assertThat(events.stream(ParticipantRemovedEvent.class).count()).isEqualTo(1),
+                () -> assertThat(events.stream(TimeBlockRemovedEvent.class).count()).isEqualTo(1),
+                () -> verify(timeBlockService).remove(ROOM_UUID, participantName)
+        );
     }
 }
