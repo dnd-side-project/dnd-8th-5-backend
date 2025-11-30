e1aca618215fcda06d18e7d49afa19b7e4d59313
dio.kim <dio.kim@daangn.com>
2025-11-30 17:12:36 +0900

    feat: ec2 swap 추가

---
+++ scripts/ec2-swap-settings.sh
@@ -0,0 +1,7 @@
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo sysctl vm.swappiness=10
+echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
