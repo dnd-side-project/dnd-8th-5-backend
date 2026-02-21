05a28702cfd8237499564917991b727ee3b34e43
dongho <dongho108@naver.com>
2026-02-21 17:07:11 +0900

    fix: API 문서 경로 허용 목록에 추가

--- src/main/java/com/dnd/modutime/core/auth/oauth/OAuth2SecurityConfig.java
+++ src/main/java/com/dnd/modutime/core/auth/oauth/OAuth2SecurityConfig.java
@@ -117,6 +117,7 @@ public class OAuth2SecurityConfig {
     public RequestMatcher permitAllMatchers() {
         return new OrRequestMatcher(
                 new AntPathRequestMatcher("/aws"),
+                new AntPathRequestMatcher("/docs/index.html"),
                 new AntPathRequestMatcher("/oauth2/kakao/callback"),
                 new AntPathRequestMatcher("/oauth2/authorization/**"),
                 new AntPathRequestMatcher("/oauth2/reissue-token"),
