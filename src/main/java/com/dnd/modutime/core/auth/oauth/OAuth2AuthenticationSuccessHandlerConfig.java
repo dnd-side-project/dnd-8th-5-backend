7537f61158ebc6ad6b1dbea9e685c978ef5b3cf5
ash <54317630+dongho108@users.noreply.github.com>
2026-03-18 01:22:49 +0900

    fix: 운영환경에서도 REFERER 기반으로 리다이렉트 (#142)
    * fix: OAuth2 핸들러 및 리졸버 설정에서 환경별 분기 제거
    
    * refactor: 운영 oauth 로깅 제거

--- src/main/java/com/dnd/modutime/core/auth/oauth/OAuth2AuthenticationSuccessHandlerConfig.java
+++ src/main/java/com/dnd/modutime/core/auth/oauth/OAuth2AuthenticationSuccessHandlerConfig.java
@@ -4,11 +4,9 @@ import com.dnd.modutime.core.auth.oauth.facade.OAuth2TokenProvider;
 import com.dnd.modutime.core.auth.oauth.facade.OAuth2TokenService;
 import com.dnd.modutime.core.auth.oauth.facade.TokenConfigurationProperties;
 import com.dnd.modutime.core.common.Constants;
-import com.dnd.modutime.core.common.ModutimeHostConfigurationProperties;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -20,8 +18,7 @@ import static org.springframework.http.HttpHeaders.REFERER;
  * <p>
  * 환경(local, dev, prod)에 따라 적절한 {@link AuthenticationSuccessHandler}를 생성합니다.
  * <p>
- * local, dev 환경: state 값에 포함된 Referer 헤더를 파싱한 후 해당 주소로 redirect
- * prod 환경: 명시적으로 지정된 프론트의 도메인 주소로 redirect
+ * 모든 환경에서 state 값에 포함된 Referer 헤더를 파싱한 후 해당 주소로 redirect
  *
  * @see OAuth2AuthenticationSuccessHandler
  */
@@ -32,7 +29,6 @@ public class OAuth2AuthenticationSuccessHandlerConfig {
 
     @Configuration
     @ConditionalOnProperty(value = CLIENTS_OAUTH2_MODE, havingValue = Constants.CLIENT_MODE_STUB, matchIfMissing = true)
-    @EnableConfigurationProperties(ModutimeHostConfigurationProperties.class)
     public static class RefererConfig {
 
         private final OAuth2TokenProvider tokenProvider;
@@ -60,19 +56,15 @@ public class OAuth2AuthenticationSuccessHandlerConfig {
 
     @Configuration
     @ConditionalOnProperty(value = CLIENTS_OAUTH2_MODE, havingValue = Constants.CLIENT_MODE_OAUTH2)
-    @EnableConfigurationProperties(ModutimeHostConfigurationProperties.class)
     public static class RealConfig {
 
-        private final ModutimeHostConfigurationProperties properties;
         private final OAuth2TokenProvider tokenProvider;
         private final OAuth2TokenService tokenService;
         private final TokenConfigurationProperties tokenConfigurationProperties;
 
-        public RealConfig(final ModutimeHostConfigurationProperties properties,
-                          final OAuth2TokenProvider tokenProvider,
+        public RealConfig(final OAuth2TokenProvider tokenProvider,
                           final OAuth2TokenService tokenService,
                           final TokenConfigurationProperties tokenConfigurationProperties) {
-            this.properties = properties;
             this.tokenProvider = tokenProvider;
             this.tokenService = tokenService;
             this.tokenConfigurationProperties = tokenConfigurationProperties;
@@ -84,7 +76,7 @@ public class OAuth2AuthenticationSuccessHandlerConfig {
                     this.tokenProvider,
                     this.tokenService,
                     this.tokenConfigurationProperties,
-                    this.properties.host().client()
+                    REFERER
             );
         }
     }
