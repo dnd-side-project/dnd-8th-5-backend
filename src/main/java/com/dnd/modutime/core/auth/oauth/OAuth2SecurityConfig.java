55708306f5087972f6a316e241ee13a5f212e6df
dongho <dongho108@naver.com>
2026-04-26 13:06:04 +0900

    fix: CORS 허용 목록에 개발 환경 도메인 추가

--- src/main/java/com/dnd/modutime/core/auth/oauth/OAuth2SecurityConfig.java
+++ src/main/java/com/dnd/modutime/core/auth/oauth/OAuth2SecurityConfig.java
@@ -222,7 +222,9 @@ public class OAuth2SecurityConfig {
         if (List.of(environment.getActiveProfiles()).contains("prod")) {
             configuration.setAllowedOriginPatterns(List.of(
                     hostProperties.host().client(),
-                    "https://modutime.site"
+                    "https://modutime.site",
+                    "https://dev.modutime.site/",
+                    "https://www.dev.modutime.site/"
             ));
         } else {
             configuration.setAllowedOriginPatterns(List.of("*"));
